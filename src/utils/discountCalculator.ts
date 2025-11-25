15196f6a816e9f19efccbefcbb89d9d30d41cdfd
Your Name <mugeta88@gmail.com>
2025-11-25 11:02:19 -0500

    created a class discountTaxCal with the method calculateDiscount() to calcuate the dollar amount off a product's price

--- src/utils/discountCalculator.ts
+++ src/utils/discountCalculator.ts
@@ -1,3 +1,14 @@
-import {Product} from "../models/Product";
+import {Product} from "../models/Product2";
 
-export class discountTaxCal 
+export class discountTaxCal extends Product {
+    constructor(id: number, title: string, description: string, price: number, discountPercentage: number) {
+        super(id, title, description, price, discountPercentage)
+    }
+
+    calculateDiscount() {
+        let discountAmount = this.price * this.discountPercentage;
+        return `$${discountAmount}`;
+
+    }
+
+}
