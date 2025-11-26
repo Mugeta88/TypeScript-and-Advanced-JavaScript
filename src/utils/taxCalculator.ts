46227a3cd017a55f611c4e580fd6dc6f0d4ba3d9
Your Name <mugeta88@gmail.com>
2025-11-26 15:01:07 -0500

    created a function calculateTax() that returns the tax amount depending on the category

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,22 +1,16 @@
 import { Product } from "../models/Product2";
 
-export class TaxCal extends Product {
-    category: string;
-
-    constructor(id: number, title: string, description: string, price: number, discountPercentage: number, category: string) {
-    super(id, title, description, price, discountPercentage)
-    this.category = category;
-
-    }
 
 
-    calculateTax(): number {
-        if (this.category === "groceries") {
+
+export function calculateTax(product: Product): number {
+        if (product.category === "groceries") {
             let taxRate = 0.03;
-            return taxRate;
+            let taxAmount = product.price * taxRate;
+            return Number(taxAmount.toFixed(2));
         } else {
             let taxRate = 0.0475;
-            return taxRate; 
+            let taxAmount = product.price * taxRate;
+            return Number(taxAmount.toFixed(2)); 
         }
     }
-}
