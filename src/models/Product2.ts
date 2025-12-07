8cc5ff5d12d4f4c1706dd4c4eadd20ef54ef5804
Your Name <mugeta88@gmail.com>
2025-12-07 17:08:42 -0500

    made a minor change

--- src/models/Product2.ts
+++ src/models/Product2.ts
@@ -1,6 +1,4 @@
-fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(console.log);
+
 
 
 export class Product {
@@ -9,14 +7,16 @@ export class Product {
     description: string;
     price: number;
     discountPercentage: number;
+    category: string;
 
 
-    constructor(id: number, title: string, description: string, price: number, discountPercentage: number){
+    constructor(id: number, title: string, description: string, price: number, discountPercentage: number, category: string){
         this.id = id;
         this.title = title;
         this.description = description;
         this.price = price;
         this.discountPercentage = discountPercentage;
+        this.category = category;
     }
 
     displayDetails(): string {
@@ -25,15 +25,16 @@ export class Product {
         ID: ${this.id} 
         Price: $${this.price} 
         Discount: ${this.discountPercentage}
+        Category: ${this.category}
         `
     }
 
-    getPriceWithDiscount(quantity: number = 1, taxRate: number = 0): string{
+    getPriceWithDiscount(quantity: number = 1, taxRate: number = 0): number{
         let discountAmount = this.price  * (this.discountPercentage / 100);
         let discountPrice = this.price - discountAmount;
         
         let subtotal = discountPrice * quantity;
         let total = subtotal * (1 + taxRate);
-        return total.toFixed(2);
+        return Number(total.toFixed(2));
     }
 }
